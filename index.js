41266b9234fa0ba8ebc21f6728bd17c80b7829f2
Nicho1812 <Nicholaslimbong8@gmail.com>
2025-05-24 14:50:37 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Jenkins Demo App!');
 });
 
-app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
-});
-
-module.exports = app; // Export for testing
+module.exports = app;
